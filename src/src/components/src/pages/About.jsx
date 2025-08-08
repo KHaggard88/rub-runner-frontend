dd2f2338fbf686f494590b0461693a67844bd56f
KHaggard88 <hagsaw@gmail.com>
2025-08-08 14:18:31 -0700

    Create About.jsx

---
+++ src/src/components/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About(){
+  return (
+    <div className="container">
+      <h1>About Us</h1>
+      <p>RUB RUNNER. We’re not going to give you that cheesy line about how we've been around since the dawn of time. We’re not even going to say these are family spice secrets passed down for generations, or that these flavor combos challenge the boundaries of what you thought food could taste like.</p>
+      <p>Instead, here’s what you need to know: RUB RUNNER is a company that makes a product—nay, a tradition—that spans across cultures and continents. Whether you cook indoors or outdoors, whether you love cooking or just love eating, these products are for you.</p>
+      <p>This veteran-owned company is offering you a straightforward product for whatever your cooking and grilling goals are—whether it’s seeing good food, smelling good food, eating good food, or showing off good food. No matter what you're aiming to achieve, we guarantee you'll have a good time sharing and laughing about the great experiences of being together… The RUB RUNNER way…</p>
+    </div>
+  )
+}
